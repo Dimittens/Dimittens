d0d499c2a57c5bc4e849f2683b8372aa07431f0b
Dimittens <dimittens.contato@gmail.com>
2024-06-11 14:51:21 -0300

    k

--- app.js
+++ app.js
@@ -1,27 +1,7 @@
-require('dotenv').config(); // Carrega variáveis de ambiente do arquivo .env
-
 const express = require('express');
-const mysql = require('mysql');
 const app = express();
 const path = require('path');
-
-// Use a porta fornecida pelo ambiente ou 3000 como padrão
 const port = process.env.APP_PORT || 3000;
-
-// Configurar a conexão com o banco de dados
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-
-db.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('Conectado ao banco de dados.');
-});
 
 // Define a engine de visualização como EJS
 app.set('view engine', 'ejs');
