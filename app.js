f64916b8602c8e343a1167738b29a482eca31656
Dimittens <dimittens.contato@gmail.com>
2024-06-11 15:56:06 -0300

    css

--- app.js
+++ app.js
@@ -5,21 +5,10 @@ const app = express();
 
 // Define a engine de visualização como EJS
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'app', 'views')); // Caminho atualizado
-
-// Log para verificar o caminho das views
-console.log('Views directory:', path.join(__dirname, 'app', 'views'));
-
-// Verifique o conteúdo do diretório 'views'
-const viewsDir = path.join(__dirname, 'app', 'views');
-console.log('Views directory contents:', fs.readdirSync(viewsDir));
-
-// Verifique o conteúdo do diretório 'views/pages'
-const pagesDir = path.join(viewsDir, 'pages');
-console.log('Pages directory contents:', fs.readdirSync(pagesDir));
+app.set('views', path.join(__dirname, 'app', 'views')); 
 
 // Servir arquivos estáticos da pasta 'public'
-app.use(express.static(path.join(__dirname, 'app', 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Rota para a página principal
 app.get('/', (req, res) => {
