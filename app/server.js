de8bbe0336b57cf066f4086bd799c2a4616e94ee
Dimittens <dimittens.contato@gmail.com>
2024-06-11 01:37:06 -0300

    server

--- app/server.js
+++ app/server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+
+// Use a porta fornecida pelo ambiente ou 3000 como padrão
+const port = process.env.PORT || 3000;
 
 // Define a engine de visualização como EJS
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views')); // Aponta para 'app/views'
+app.set('views', path.join(__dirname, 'views'));
 
 // Servir arquivos estáticos da pasta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Rota para a página principal
 app.get('/', (req, res) => {
-    res.render('pages/index'); // Renderiza 'app/views/pages/index.ejs'
+    res.render('pages/index');
 });
 
 // Inicia o servidor
