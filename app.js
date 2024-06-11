296a4e9d8b3af9728f1413004353529227416530
Dimittens <dimittens.contato@gmail.com>
2024-06-11 11:14:53 -0300

    app js

--- app.js
+++ app.js
@@ -5,24 +5,6 @@ const mysql = require('mysql');
 const app = express();
 const path = require('path');
 
-// Use a porta fornecida pelo ambiente ou 3000 como padrão
-const port = process.env.APP_PORT || 3000;
-
-// Configurar a conexão com o banco de dados
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-
-db.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('Conectado ao banco de dados.');
-});
-
 // Define a engine de visualização como EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
