355b138de76daa84a76929f8035b03f08dacfbfe
Dimittens <dimittens.contato@gmail.com>
2024-06-25 02:20:52 -0300

    chat finalizado + comunidade finalizado (falta a responsividade)

---
+++ app/public/js/chat.js
@@ -0,0 +1,5 @@
+/* Menu side-bar abertura no botão */
+
+document.getElementById('open_btn').addEventListener('click', function () {
+    document.getElementById('sidebar').classList.toggle('open-sidebar');
+});
